3c10aa2e4ec53705ddd8d36686e6c4381026001b
Aryan Meher <aryanmeher5000@gmail.com>
2026-04-03 23:02:08 +0530

    suga coating

--- src/controller/transaction.controller.ts
+++ src/controller/transaction.controller.ts
@@ -26,20 +26,20 @@ export async function getTransactionsByCriteriaController(req: Request, res: Res
     data.page,
   );
 
-  res.status(200).json({ ...result });
+  res.status(200).json({ data: result });
 }
 
 export async function getTransactionByIdContoller(req: Request, res: Response) {
   const transactionId = extractId("transaction", req.params.id);
   const transaction = await getTransactionById(transactionId);
-  res.status(200).json({ transaction });
+  res.status(200).json({ data: { transaction } });
 }
 
 export async function createTransactionController(req: Request, res: Response) {
   const { amount, category, type, description, date } = req.body;
   const userId = req.user.sub;
   const transaction = await createTransaction(amount, category, type, description, new Date(date), userId);
-  res.status(201).json({ transaction });
+  res.status(201).json({ data: { transaction } });
 }
 
 export async function updateTransactionController(req: Request, res: Response) {
@@ -47,12 +47,12 @@ export async function updateTransactionController(req: Request, res: Response) {
   const adminId = req.user.sub;
   const { amount, type, category, description, date } = req.body;
   const transaction = await updateTransaction(transactionId, adminId, amount, type, category, description, date);
-  res.status(200).json({ transaction });
+  res.status(200).json({ data: { transaction } });
 }
 
 export async function deleteTransactionController(req: Request, res: Response) {
   const transactionId = extractId("transaction", req.params.id);
   const adminId = req.user.sub;
   const transaction = await deleteTransaction(transactionId, adminId);
-  res.status(200).json({ transaction });
+  res.status(200).json({ data: { transaction } });
 }
